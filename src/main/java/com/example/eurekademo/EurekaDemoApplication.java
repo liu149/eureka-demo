009082e446fc7472d11c01e034d4f93edc4f4ce0
liu149 <liuqi1212@outlook.com>
2020-12-21 22:32:40 +0800

    eureka server

--- src/main/java/com/example/eurekademo/EurekaDemoApplication.java
+++ src/main/java/com/example/eurekademo/EurekaDemoApplication.java
@@ -2,10 +2,10 @@ package com.example.eurekademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-//@EnableEurekaServer
+@EnableEurekaServer
 public class EurekaDemoApplication {
 
 	public static void main(String[] args) {
