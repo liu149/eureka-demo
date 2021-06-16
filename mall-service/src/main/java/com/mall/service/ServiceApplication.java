29535aeeee49deee9dc3b79791007b16fdd334f5
liu149 <liuqi1212@outlook.com>
2021-06-16 22:40:49 +0800

    优化gradle

--- mall-service/src/main/java/com/mall/service/ServiceApplication.java
+++ mall-service/src/main/java/com/mall/service/ServiceApplication.java
@@ -1,15 +1,14 @@
-package com.mall.eureka;
+package com.mall.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
-@EnableEurekaServer
-public class EurekaDemoApplication {
+@EnableEurekaClient
+public class ServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaDemoApplication.class, args);
+		SpringApplication.run(ServiceApplication.class,args);
 	}
-
 }
